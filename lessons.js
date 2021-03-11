3e67c71cc54212041e6bf7a5dd3038a157213f8a
Gift <gyftochuwa@gmail.com>
2021-03-12 00:54:54 +0100

    firstcommit

---
+++ lessons.js
@@ -0,0 +1,83 @@
+let lessondata=[
+    {
+        id: 1001,
+        subject: "Math",
+        location: "Egypt",
+        price: 100,
+        space: 5,
+        image: "images/math.jpg"
+    },
+    {
+        id: 1002,
+        subject: "Physics",
+        location: "Rome",
+        price: 200,
+        space: 5,
+        image: "images/physics.png"
+    },
+    {
+        id: 1003,
+        subject: "Chemistry",
+        location: "Germany",
+        price: 300,
+        space: 5,
+        image: "images/chem.png"
+    },
+    {
+        id: 1004,
+        subject: "Biology",
+        location: "China",
+        price: 400,
+        space: 5,
+        image: "images/bio.jpg"
+    },
+    {
+        id: 1005,
+        subject: "Engish",
+        location: "England",
+        price: 500,
+        space: 5,
+        image: "images/english.jpg"
+    },
+    {
+        id: 1006,
+        subject: "Geography",
+        location: "Namibia",
+        price: 600,
+        space: 5,
+        image: "images/geo.jpg"
+    },
+    {
+        id: 1007,
+        subject: "Music",
+        location: "Nigeria",
+        price: 700,
+        space: 5,
+        image: "images/music.jpg"
+    },
+    {
+        id: 1008,
+        subject: "Photography",
+        location: "USA",
+        price: 800,
+        space: 5,
+        image: "images/photo.jpg"
+    },
+    {
+        id: 1009,
+        subject: "Spanish",
+        location: "Columbia",
+        price: 900,
+        space: 5,
+        image: "images/spans.jpg"
+    },
+    {
+        id: 1010,
+        subject: "Art",
+        location: "France",
+        price: 1000,
+        space: 5,
+        image: "images/art.jpg"
+    }
+]
+//export default lessondata 
